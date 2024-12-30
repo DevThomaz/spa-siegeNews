a7b969ef1f66523aabe7af1258a2a9efe938ac04
DevThomaz <socialdevguithz@gmail.com>
2024-12-29 22:55:22 -0300

    feat: :sparkles: Add js-cookie

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Card } from "../../components/Card/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { getAllPosts, getTopPost } from "../../services/postsServices.js";
 import { HomeBody, HomeHeader } from "./HomeStyled.jsx";
+import Cookies from "js-cookie";
 
 export default function Home(){
     const [posts, setPosts] = useState([])
@@ -19,7 +20,8 @@ export default function Home(){
 
     useEffect(() => {
         findPost();
-    },[])
+        console.log(Cookies.get("token"));
+    },[]);
 
 
     return (
